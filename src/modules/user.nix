fc0f14ea27ed1a49aabdda3dc32e8b3e742e93c5
Josip Vulic <jovulic@gmail.com>
2025-05-29 22:15:15 -0400

    ftr(user): configure users with correct file permissions

--- src/modules/user.nix
+++ src/modules/user.nix
@@ -2,41 +2,37 @@
 let
   root = "root";
   nomad = "nomad";
-  etcPasswdFile = pkgs.writeTextFile {
-    name = "etc-passwd";
-    text = ''
-      ${root}:x:0:0:${root}:/${root}:/bin/sh
-      ${nomad}:!:1001:1001:${nomad}:/home/${nomad}:/bin/sh
-    '';
-    destination = "/etc/passwd";
-  };
-  etcGroupFile = pkgs.writeTextFile {
-    name = "etc-group";
-    text = ''
-      ${root}:x:0:
-      ${nomad}:x:1001:
-    '';
-    destination = "/etc/group";
-  };
-  etcShadowFile = pkgs.writeTextFile {
-    name = "etc-shadow";
-    text = ''
-      ${root}:*:19700:0:99999:7:::
-      ${nomad}:*:19700:0:99999:7:::
-    '';
-    destination = "/etc/shadow";
-  };
+  etcPasswdFilePath = "/etc/passwd";
+  etcGroupFilePath = "/etc/group";
+  etcShadowFilePath = "/etc/shadow";
 in
 {
   container = {
     root = {
       paths = [
         pkgs.shadow
-        etcPasswdFile
-        etcGroupFile
-        etcShadowFile
       ];
       exec = ''
+        mkdir -p /etc
+
+        cat > ${etcPasswdFilePath} <<EOF
+        ${root}:x:0:0:${root}:/${root}:/bin/nologin
+        ${nomad}:x:1001:1001:${nomad}:/home/${nomad}:/bin/sh
+        EOF
+        chmod 0644 ${etcPasswdFilePath}
+
+        cat > ${etcGroupFilePath} <<EOF
+        ${root}:x:0:
+        ${nomad}:x:1001:
+        EOF
+        chmod 0644 ${etcGroupFilePath}
+
+        cat > ${etcShadowFilePath} <<EOF
+        ${root}:*:19700:0:99999:7:::
+        ${nomad}:*:19700:0:99999:7:::
+        EOF
+        chmod 0600 ${etcShadowFilePath}
+
         mkdir -p /${root}
 
         mkdir -p /home/${nomad}
