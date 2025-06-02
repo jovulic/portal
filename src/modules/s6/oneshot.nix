ccf7b78e793d0919fd085e70747b4cf53cd4845c
Josip Vulic <jovulic@gmail.com>
2025-06-02 18:02:40 -0400

    ftr(s6): add oneshot expression

---
+++ src/modules/s6/oneshot.nix
@@ -0,0 +1,84 @@
+{ pkgs, lib, ... }:
+{
+  build =
+    {
+      name,
+      type ? "oneshot",
+      upScript,
+      downScript ? ''
+        if test "$1" -eq 256 ; then
+          err=$((128 + $2))
+        else
+          err="$1"
+        fi
+        echo "$err" > /run/s6-linux-init-container-results/exitcode
+      '',
+      dependencies ? [ ],
+    }:
+    lib.filterAttrs (_: attrValue: attrValue != null) {
+      # The "user" file marks the service as part of the user bundle (to be run
+      # with the container).
+      user = pkgs.writeTextFile {
+        name = "${name}-user";
+        text = "";
+        destination = "/etc/s6-overlay/s6-rc.d/user/contents.d/${name}";
+      };
+
+      # The "type" file defines the way the service runs. Usually longrun, but
+      # oneshot is an option too.
+      type = pkgs.writeTextFile {
+        name = "${name}-type";
+        text = type;
+        destination = "/etc/s6-overlay/s6-rc.d/${name}/type";
+      };
+
+      # The "down" file defines what should be executed when the oneshot starts.
+      up = pkgs.writeTextFile {
+        name = "${name}-up";
+        text = ''
+          #!${pkgs.execline}/bin/execlineb -P
+          ${upScript}
+        '';
+        destination = "/etc/s6-overlay/s6-rc.d/${name}/up";
+        executable = true;
+      };
+
+      # The "down" file defines what should be executed when the oneshot exits.
+      down =
+        if downScript != null then
+          pkgs.writeTextFile {
+            name = "${name}-down";
+            text = ''
+              #!${pkgs.bash}/bin/bash
+              ${downScript}
+            '';
+            destination = "/etc/s6-overlay/s6-rc.d/${name}/finish";
+            executable = true;
+          }
+        else
+          null;
+
+      # A "dependency" file defines the order that services should be started.
+      dependencies = lib.listToAttrs (
+        map (
+          depName:
+          lib.nameValuePair depName (
+            pkgs.writeTextFile {
+              name = "${name}-${depName}-dependency";
+              text = "";
+              destination = "/etc/s6-overlay/s6-rc.d/${name}/dependencies.d/${depName}";
+            }
+          )
+        ) dependencies
+      );
+    };
+  listFiles =
+    service:
+    let
+      # Get the top-level derivations, minus dependencies as we need to
+      # navigate one level down to get all the text files.
+      tops = lib.attrValues (lib.filterAttrs (name: _: name != "dependencies") service);
+      deps = lib.attrValues service.dependencies;
+    in
+    tops ++ deps;
+}
