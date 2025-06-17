61763301c39696f1e289c258bda8e313638e2482
Josip Vulic <jovulic@gmail.com>
2025-06-17 15:54:28 -0400

    upd(fonts): simplify fonts setup

--- src/modules/fonts.nix
+++ src/modules/fonts.nix
@@ -11,26 +11,11 @@
 
         # Setup font configuration.
         mkdir -p /etc/fonts 
-
-        # Link the main fonts.conf file.
         ln -sf "${pkgs.fontconfig.out}/etc/fonts/fonts.conf" /etc/fonts/fonts.conf
-
-        # Link the conf.d directory, which contains various font configuration
-        # snippets. This is crucial for default system-wide font
-        # configurations.
         ln -sf "${pkgs.fontconfig.out}/etc/fonts/conf.d" /etc/fonts/conf.d
 
         # Setup font cache.
-        mkdir -p /var/cache/fontconfig
-
-        # Ensure font cache is up-to-date after all files are in place.
-        # /etc/fonts/fonts.conf should be present from pkgs.fontconfig.
-        if [ -d /usr/share/fonts ]; then
-          echo "Updating font cache..."
-          fc-cache -frsv
-        else
-          echo "No /usr/share/fonts directory found, skipping fc-cache."
-        fi
+        fc-cache -frsv
       '';
     };
   };
